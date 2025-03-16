acfb6af5511a7d0a5655147904fce752684a1f1a
Roman Kapuścik <70838513+RomanKapuscik@users.noreply.github.com>
2025-03-16 16:50:08 +0100

    Walidacfja wejscia, czy jest prawidlowym url

--- expand_api.py
+++ expand_api.py
@@ -4,22 +4,21 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
+from starlette.responses import RedirectResponse
+
 # Database setup
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@db/url_db")
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-# URL model
 class URL(Base):
     __tablename__ = "urls"
     hash = Column(String, primary_key=True, index=True)
     original_url = Column(String, nullable=False)
 
-# Create tables
 Base.metadata.create_all(bind=engine)
 
-# FastAPI app
 app = FastAPI()
 
 @app.get("/expand/{url_hash}")
@@ -30,4 +29,5 @@ def expand_url(url_hash: str):
     db.close()
     if not db_url:
         raise HTTPException(status_code=404, detail="URL not found")
-    return {"original_url": db_url.original_url}
+
+    return RedirectResponse(db_url.original_url)
--- shorten_api.py
+++ shorten_api.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 from sqlalchemy import Column, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -11,20 +11,24 @@ engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-# URL model
 class URL(Base):
     __tablename__ = "urls"
     hash = Column(String, primary_key=True, index=True)
     original_url = Column(String, nullable=False)
 
-# Create tables
 Base.metadata.create_all(bind=engine)
 
-# FastAPI app
 app = FastAPI()
 
 class URLRequest(BaseModel):
     url: str
+
+    @field_validator("url", mode="before")
+    @classmethod
+    def validate_url(cls, url: str) -> str:
+        if not url.startswith(("http://", "https://")):
+            raise ValueError("URL musi zaczynać się od http:// lub https://")
+        return url
 
 def generate_hash(url: str) -> str:
     return hashlib.sha256(url.encode()).hexdigest()[:8]
@@ -33,7 +37,7 @@ def generate_hash(url: str) -> str:
 def shorten_url(request: URLRequest):
     """Shorten URL and save it in db."""
     db = SessionLocal()
-    url_hash = generate_hash(request.url)
+    url_hash = generate_hash(str(request.url))
     db_url = db.query(URL).filter(URL.hash == url_hash).first()
     if not db_url:
         db_url = URL(hash=url_hash, original_url=request.url)
